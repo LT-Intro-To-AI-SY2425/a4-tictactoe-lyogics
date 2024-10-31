8c3fbf71f1401b6f0c0557454a8808d8d9df2c13
lyogics <lyogi@cps.edu>
2024-10-31 11:32:23 -0500

    updated notes

--- notes.py
+++ notes.py
@@ -3,37 +3,37 @@
 # (define-struct dog [fur_color name age favorite_food])
 
 class Dog:
-    # functions that start with __ are not intended to be called directly
-    def __init__(self, n = "", fc = "", a = 0, ff = ""):
-        """Creates an instance of the dog class and sets attributes"""
+    species = "canis lupis familiaris"
+    def __init__(self, n = "no name", a = 0):
         self.name = n
-        self.fur_color = fc
         self.age = a
-        self.favorite_food = ff
+        self.fetch_count = 0
+    
+    def __str__(self) -> str:
+        s = f"{self.name} is {self.age} years old"
+        return s
+
+    def play_fetch(self, num_fetch):
+        self.fetch_count += num_fetch
+
+    def play_fetch_once(self):
+        self.fetch_count += 1
+    def reset_fetch(self):
         self.fetch_count = 0
 
-        def __str__(self) -> str:
-            """Returns a string representation of a dog"""
-            s = "Dog's name is " + self.name + "\n"
-            s += "and age is " + str(self.age) + "\n"
-            s += "and fur color is " + self.fur_color + "\n"
-            s += "they have played fetch " + str(self.fetch_count) + " times\n"
-            return s
-        
-        def play_fetch(self, num_times):
-            self.fetch_count += num_times
-        
-        def paint_dog(self, color):
-            self.fur_color = color
+# instances of the Dog class
+logan = Dog("logan", 8)
+cookie = Dog()
+maple = Dog("maple", 1)
 
-        mydog = Dog("logan", "black", 7, "salmon")
-        chrisdog = Dog("luna", "black and white", 6, "tortillas")
+print(logan.fetch_count)
+# print(cookie)
+# print(maple)
 
-        print(mydog)
-        print(chrisdog)
+logan.play_fetch(4)
 
-        mydog.play_fetch(20)
-        chrisdog.play_fetch(3)
+print(logan.fetch_count)
 
-        print(mydog)
-        print(f"{chrisdog.name} has played fetch {chrisdog.fetch_count} times")
+logan.play_fetch(8)
+
+print(logan.fetch_count)
